39e2a758ad17c1796a4c2779a6e668eeb0a03591
markdamolotov <markdamolotov@gmail.com>
2025-06-01 14:23:24 +0300

    testing lint-staged

--- webapp/src/appp.tsx
+++ webapp/src/appp.tsx
@@ -1,7 +1,5 @@
 import { AllIdeasPage } from './pages/allIdeasPage'
 
-const x:number = '1'
-
 export const App = () => {
   return (
     <div>
--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -2,7 +2,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { App } from './app'
+import { App } from './appp'
 import { queryClient } from './lib/trpc'
 
 createRoot(document.querySelector('#root')!).render(
