9197153a9c8f203c9417c112ba50dda802902c5a
markdamolotov <markdamolotov@gmail.com>
2025-06-03 22:36:54 +0300

    add zod

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,5 +1,6 @@
 import { initTRPC } from '@trpc/server'
 import _ from 'lodash'
+import { z } from 'zod/v4'
 
 const ideas = _.times(100, (index) => ({
   description: `Description of idea ${index}`,
@@ -14,6 +15,21 @@ const ideas = _.times(100, (index) => ({
 const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
+  getIdea: trpc.procedure
+    .input(
+      z.object({
+        ideaNick: z.string()
+      })
+    )
+    .query(({ input }) => {
+      const idea = ideas.find((idea) => idea.nick === input.ideaNick)
+
+      if (!idea) {
+        throw new Error(`Idea ${input.ideaNick} not found`)
+      }
+
+      return { idea: idea || undefined }
+    }),
   getIdeas: trpc.procedure.query(() => {
     return {
       ideas: ideas.map((idea) => _.pick(idea, ['nick', 'name', 'description']))
--- webapp/src/pages/viewIdeaPage/index.tsx
+++ webapp/src/pages/viewIdeaPage/index.tsx
@@ -1,19 +1,34 @@
+import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router'
 
 import type { ViewIdeaRouteParameters } from '@/shared/lib/routes'
 
+import { trpc } from '@/shared/lib/trpc'
+
 export const ViewIdeaPage = () => {
   const { ideaNick } = useParams() as ViewIdeaRouteParameters
 
+  const { data, error, isError, isFetching, isLoading } = useQuery(
+    trpc.getIdea.queryOptions({ ideaNick })
+  )
+
+  if (isLoading || isFetching) {
+    return <div>Loading...</div>
+  }
+
+  if (isError) {
+    return <div>{error.message}</div>
+  }
+
+  if (!data?.idea) {
+    return <div>No idea found</div>
+  }
+
   return (
     <div>
-      <h1>{ideaNick}</h1>
-      <p>Description of idea</p>
-      <div>
-        <p>Text paragraph 1</p>
-        <p>Text paragraph 2</p>
-        <p>Text paragraph 3</p>
-      </div>
+      <h1>{data.idea.name}</h1>
+      <p>{data.idea.description}</p>
+      <div dangerouslySetInnerHTML={{ __html: data.idea.text }} />
     </div>
   )
 }
