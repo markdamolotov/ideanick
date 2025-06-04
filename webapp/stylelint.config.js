9b675112d8f46f1e4c827a290c2682e138e5c48a
markdamolotov <markdamolotov@gmail.com>
2025-06-04 23:07:13 +0300

    add check style before commit

--- webapp/stylelint.config.js
+++ webapp/stylelint.config.js
@@ -1,4 +1,14 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier-scss']
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-prettier-scss',
+    'stylelint-config-idiomatic-order'
+  ],
+  plugins: [
+    'stylelint-scss',
+    'stylelint-order',
+    'stylelint-declaration-block-no-ignored-properties'
+  ]
 }
